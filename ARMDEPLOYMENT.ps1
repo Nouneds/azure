3851107e3db7ff91c0d28e0780a9f5e0282aa51f
Nouneds <124002669+Nouneds@users.noreply.github.com>
2023-01-30 23:35:06 +0100

    Add files via upload

---
+++ ARMDEPLOYMENT.ps1
@@ -0,0 +1,48 @@
+﻿$SubscriptionID = #subscription parameter
+$TenantName = Get-aztenant | select -ExpandProperty Name
+$mwrg = $tenantname + 'MW-RG'
+$mwloganalytics = $tenantName +'MW-LogAnalytics'
+$Location = 'West Europe'
+
+#Checks and updates AZ module
+Function CheckInstalledModule
+{
+$tabModules = @()
+$modules = (Get-InstalledModule).Name
+
+Foreach ($m in $modules)
+{
+$tabModules += $modules
+}
+if ($tabModules -notcontains "AZ")
+{
+Install-Module -Name Az -Scope CurrentUser -Repository PSGallery -Force
+Write-Host "Installing AzureAD module" -ForegroundColor Cyan
+}
+if ($tabModules -contains "AZ")
+{
+Write-Host "AZ module is already installed" -ForegroundColor Green
+}
+}
+CheckInstalledModule
+
+Connect-azaccount
+Set-AzContext -subscription $SubscriptionID
+
+#Resource group pre-creation
+New-AzResourceGroup -Name $mwrg -Location $Location -ErrorAction Stop | Write-Host ($mwrg + ' Created Successfully') -BackgroundColor Green
+New-AzResourceGroup -Name $mwloganalytics -Location $Location -ErrorAction Stop | Write-Host ($mwloganalytics + ' Created Successfully') -BackgroundColor Green
+
+
+#ARM Deployment
+New-AzResourceGroupDeployment -ResourceGroupName $mwrg `
+-Name `
+-TemplateFile `
+-TemplateParameterFile `
+-DeploymentDebugLogLevel RequestContent
+
+New-AzResourceGroupDeployment -ResourceGroupName $mwloganalytics `
+-Name `
+-TemplateFile `
+-TemplateParameterFile `
+-DeploymentDebugLogLevel RequestContent
